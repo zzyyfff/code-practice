bd7f2e928f15104e74c5f42d8ae374dcd262c31b
zzyyfff <jonathan.marcus@gmail.com>
2019-05-15 14:09:45 -0400

    hr9

--- hr/09-recursion.js
+++ hr/09-recursion.js
@@ -1,43 +1,43 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
+const fs = require('fs')
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
 
-let inputString = '';
-let currentLine = 0;
+let inputString = ''
+let currentLine = 0
 
 process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
+  inputString += inputStdin
+})
 
 process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+  inputString = inputString.replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''))
 
-    main();
-});
+  main()
+})
 
-function readLine() {
-    return inputString[currentLine++];
+function readLine () {
+  return inputString[currentLine++]
 }
 
 // Complete the factorial function below.
-function factorial(n) {
-    if (n < 2) return 1
-    return n * factorial(n - 1)
+function factorial (n) {
+  if (n < 2) return 1
+  return n * factorial(n - 1)
 }
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+function main () {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH)
 
-    const n = parseInt(readLine(), 10);
+  const n = parseInt(readLine(), 10)
 
-    let result = factorial(n);
+  const result = factorial(n)
 
-    ws.write(result + "\n");
+  ws.write(result + '\n')
 
-    ws.end();
+  ws.end()
 }
