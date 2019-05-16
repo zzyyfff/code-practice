a1ef41e499f3915796bbb50b735764c114214236
zzyyfff <jonathan.marcus@gmail.com>
2019-05-16 00:26:38 -0400

    hr17

---
+++ hr/17-moreexceptions.js
@@ -0,0 +1,44 @@
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+let input_stdin = ''
+let input_stdin_array = ''
+let input_currentline = 0
+
+process.stdin.on('data', function (data) {
+  input_stdin += data
+})
+
+process.stdin.on('end', function () {
+  input_stdin_array = input_stdin.split('\n')
+  main()
+})
+function readLine () {
+  return input_stdin_array[input_currentline++]
+}
+
+// Write your code here
+class Calculator {
+  power (n, p) {
+    if (n < 0 || p < 0) {
+      throw 'n and p should be non-negative'
+    }
+    return n ** p
+  }
+}
+
+function main () {
+  const myCalculator = new Calculator()
+  let T = parseInt(readLine())
+  while (T-- > 0) {
+    const num = (readLine().split(' '))
+    try {
+      const n = parseInt(num[0])
+      const p = parseInt(num[1])
+      const ans = myCalculator.power(n, p)
+      console.log(ans)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
